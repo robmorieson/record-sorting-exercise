953d3531237587000a49fec906fb468723558c46
Rob Morieson <rob@electricanimals.com>
2021-04-21 18:27:04 +1000

    Sort records by suburb, ascending

--- components/Records.tsx
+++ components/Records.tsx
@@ -8,9 +8,21 @@ type Props = {
 export const Records = ({ records }: Props) => {
   return (
     <div>
-      {records.map((record: RecordType) => (
-        <Record key={record._id} record={record} />
-      ))}
+      {records
+        .sort((a: RecordType, b: RecordType) => {
+          const sortA = a["Suburb - Incident"];
+          const sortB = b["Suburb - Incident"];
+          if (sortA < sortB) {
+            return -1;
+          }
+          if (sortA > sortB) {
+            return 1;
+          }
+          return 0;
+        })
+        .map((record: RecordType) => (
+          <Record key={record._id} record={record} />
+        ))}
     </div>
   );
 };
