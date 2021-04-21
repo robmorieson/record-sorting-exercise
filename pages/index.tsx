61d9cb9ab0d08e3cf44282232c78bf5f56e02bea
Rob Morieson <rob@electricanimals.com>
2021-04-21 14:45:19 +1000

    Remove boilerplate

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Record Sorting Exercise</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main></main>
+    </div>
+  );
+}
