482859878e54773b4667093663b078da884c067e
Rob Morieson <rob@electricanimals.com>
2021-04-21 21:56:57 +1000

    Unit tests for Record component

---
+++ tests/test-data.ts
@@ -0,0 +1,24 @@
+import { RecordType } from "../types/record";
+
+export const records: RecordType[] = [
+  {
+    _id: 1,
+    "Suburb - Incident": "ALPHA",
+    "Reported Date": "01/01/2021",
+    "Postcode - Incident": "5000",
+    "Offence count": "1",
+    "Offence Level 1 Description": "A-1",
+    "Offence Level 2 Description": "A-2",
+    "Offence Level 3 Description": "A-3",
+  },
+  {
+    _id: 2,
+    "Suburb - Incident": "OMEGA",
+    "Reported Date": "01/01/2021",
+    "Postcode - Incident": "5000",
+    "Offence count": "1",
+    "Offence Level 1 Description": "B-1",
+    "Offence Level 2 Description": "B-2",
+    "Offence Level 3 Description": "B-3",
+  },
+];
