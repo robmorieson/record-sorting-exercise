f9a67b07db95a6ed8ebc3589e904119193ddd5ea
Rob Morieson <rob@electricanimals.com>
2021-04-22 14:36:39 +1000

    Update tests for new accordion flow

--- tests/Record.test.tsx
+++ tests/Record.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
+import { render } from "@testing-library/react";
 
 import { Record } from "../components/Record";
 import { records } from "./test-data";
@@ -27,21 +27,5 @@ describe("<Record />", () => {
     expect(count).not.toBeVisible();
     expect(level1).not.toBeVisible();
     expect(level3).not.toBeVisible();
-  });
-
-  test('On "view more" click; expanded data to be visible', () => {
-    const record = records[0];
-    const { getByText, getByRole } = render(<Record record={record} />);
-
-    const button = getByRole("button");
-    const count = getByText(record["Offence count"]);
-    const level1 = getByText(record["Offence Level 1 Description"]);
-    const level3 = getByText(record["Offence Level 3 Description"]);
-
-    fireEvent.click(button);
-    // Assertions - the following to now be visible
-    expect(count).toBeVisible();
-    expect(level1).toBeVisible();
-    expect(level3).toBeVisible();
   });
 });
--- tests/Records.test.tsx
+++ tests/Records.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 
 import { Records } from "../components/Records";
 import { records } from "./test-data";
@@ -9,5 +9,21 @@ describe("<Records />", () => {
     const { getAllByTestId } = render(<Records records={records} />);
     const renderedRecords = getAllByTestId("record");
     expect(renderedRecords).toHaveLength(2);
+  });
+
+  test('On "view more" click; expanded data to be visible', () => {
+    const record = records[0];
+    const { getByText, getByRole } = render(<Records records={[record]} />);
+
+    const button = getByRole("button");
+    const count = getByText(record["Offence count"]);
+    const level1 = getByText(record["Offence Level 1 Description"]);
+    const level3 = getByText(record["Offence Level 3 Description"]);
+
+    fireEvent.click(button);
+    // Assertions - the following to now be visible
+    expect(count).toBeVisible();
+    expect(level1).toBeVisible();
+    expect(level3).toBeVisible();
   });
 });
